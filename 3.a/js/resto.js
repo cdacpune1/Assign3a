ee1e44e612966d214181535d5933874629dcbd07
cdacpune1 <50952547+cdacpune1@users.noreply.github.com>
2020-01-09 19:54:17 +0530

    Add files via upload

---
+++ 3.a/js/resto.js
@@ -0,0 +1,16 @@
+
+
+
+function fun(){
+    console.log("Helloword", document.getElementById("tab1"));
+     document.getElementById("tab1").style.visibility = "visible";
+ }
+ 
+ function hideTable(){
+
+         document.getElementById("tab1").style.visibility = "hidden";
+ }
+
+
+
+
